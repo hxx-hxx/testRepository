1a60d441f0ece055b48d1aa4de7c19f12e84b772
hxx <huoxiangxiang@chinanews.com.cn>
2018-04-28 16:40:40 +0800

    4rd push

--- src/test01/TestGit002.java
+++ src/test01/TestGit002.java
@@ -4,5 +4,6 @@ public class TestGit002 {
     public void m1(){
         System.out.println("my 2nd gitDemo...");
         System.out.println("333333333333");
+        System.out.println("444444444444");
     }
 }
