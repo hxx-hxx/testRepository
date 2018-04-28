e58026fc690014b396f3dceac9818f338f1321f4
hxx <huoxiangxiang@chinanews.com.cn>
2018-04-28 16:42:27 +0800

    6 push

--- src/test01/TestGit002.java
+++ src/test01/TestGit002.java
@@ -6,5 +6,6 @@ public class TestGit002 {
         System.out.println("333333333333");
         System.out.println("444444444444");
         System.out.println("5555555");
+        System.out.println("666666666666");
     }
 }
